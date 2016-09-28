12ef57b7d55bbac7134a80e5280e3299ccd4d1a1
Nagala, Rajesh (US - Bengaluru) <rnagala@deloitte.com>
2016-09-28 14:40:16 +0530

    initial push

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Nagala, Rajesh (US - Bengaluru) on 9/25/15.
+//  Copyright © 2015 Nagala, Rajesh (US - Bengaluru). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Nagala, Rajesh (US - Bengaluru) on 9/25/15.
+//  Copyright © 2015 Nagala, Rajesh (US - Bengaluru). All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
